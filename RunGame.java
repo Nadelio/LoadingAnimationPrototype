e5d23f59f1ba8e14c1fcb3b93131fc20d1c9d508
Nadelio <138712393+Nadelio@users.noreply.github.com>
2024-01-24 14:17:31 -0600

    Added credit for RunGame.java code

--- RunGame.java
+++ RunGame.java
@@ -1,17 +1,20 @@
 import java.io.Console;
 import java.io.IOException;
 import java.awt.GraphicsEnvironment;
-
+// credit to https://stackoverflow.com/a/29138847/21975226 for this code! :)
 public class RunGame
 {
     public static void main(String[] args)
     {
         // this is what will be compiled into a .jar file that calls the .class files that then run the animation.
-            Console console = System.console();
-        if(console == null && !GraphicsEnvironment.isHeadless()){
+        Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()
+        {
             String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
-            try {Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});}catch(IOException e) {}
-        }else{
+            try {Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});}catch(IOException e){}
+        }
+        else
+        {
             Main.main(new String[0]);
             System.out.println("Program has ended, please type 'exit' to close the console");
         }
