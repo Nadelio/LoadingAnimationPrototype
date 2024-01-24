cccc1ee91916386de3e5810d7a61d84cd816f09e
Nadelio <carefulsniffle@gmail.com>
2024-01-24 14:05:22 -0600

    Added Variant Loading Animation

--- Animation.java
+++ Animation.java
@@ -1,7 +1,10 @@
 public class Animation
 {
+    public static boolean animationComplete;
+
     public static void loadAnimText()
     {
+        animationComplete = false;
         String loadPercent = "";
         String[] spinWheelFrames = {"|", "/", "--", "\\", "|", "/", "--", "\\"};
         int loadPercentNum = 0;
@@ -16,14 +19,46 @@ public class Animation
             loadPercent = loadPercent + "=";
             loadPercentNum++;
         }
-        clearScreen();
-        System.out.println("|" + loadPercent + "|");
-        System.out.println("Load Complete!");
+        loadComplete("|" + loadPercent + "|");
     }
 
     public static void clearScreen()
     {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+
+    public static void loadComplete()
+    {
+        clearScreen();
+        System.out.println("Load Complete!");
+        animationComplete = true;
+    }
+
+    public static void loadComplete(String loadMessage)
+    {
+        clearScreen();
+        System.out.println(loadMessage);
+        System.out.println("Load Complete!");
+        animationComplete = true;
+    }
+
+    public static void loadAnimTextVariant()
+    {
+        animationComplete = false;
+        String[] loadingAnimFrames = {".", "..", "...", ""};
+        int loadPercent = 0;
+
+        while(loadPercent < 6)
+        {
+            for(int i = 0; i < loadingAnimFrames.length; i++)
+            {
+                clearScreen();
+                System.out.print("Loading" + loadingAnimFrames[i]);
+                try{Thread.sleep(200);}catch(Exception e){}
+            }
+            loadPercent++;
+        }
+        loadComplete();
     }
 }
--- Main.java
+++ Main.java
@@ -3,5 +3,14 @@ public class Main
     public static void main(String [] args)
     {
         Animation.loadAnimText();
+        try{Thread.sleep(1000);}catch(Exception e){}
+        while(true)
+        {
+            if(Animation.animationComplete)
+            {
+                Animation.loadAnimTextVariant();
+                break;
+            }
+        }
     }
 }
