321d00b9e5a8ea4af61376c2b0885604f98c390c
Nadelio <carefulsniffle@gmail.com>
2024-01-24 14:13:02 -0600

    Added Compiled Prototype and .jar file that automatically runs the .class file (WINDOWS ONLY)

---
+++ RunGame.java
@@ -0,0 +1,19 @@
+import java.io.Console;
+import java.io.IOException;
+import java.awt.GraphicsEnvironment;
+
+public class RunGame
+{
+    public static void main(String[] args)
+    {
+        // this is what will be compiled into a .jar file that calls the .class files that then run the animation.
+            Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            try {Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});}catch(IOException e) {}
+        }else{
+            Main.main(new String[0]);
+            System.out.println("Program has ended, please type 'exit' to close the console");
+        }
+    }    
+}
